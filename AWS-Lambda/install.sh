37da219d4b76680a22d61e3661760cb888436cae
sangam14 <smbiradar14@gmail.com>
2024-08-29 16:43:05 +0530

    update-script

--- AWS-Lambda/install.sh
+++ AWS-Lambda/install.sh
@@ -13,18 +13,14 @@ gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
 # Verify the fingerprint of the key
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
+sudo echo ; zcat <( CURRR_VER=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version') ; curl -q "https://releases.hashicorp.com/terraform/${CURRR_VER#?}/terraform_${CURRR_VER#?}_linux_amd64.zip" ) | sudo tee /usr/local/bin/terraform > /dev/null ; sudo chmod +x /usr/local/bin/terraform
 
-# Add the HashiCorp repository
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 # Update package list and install Terraform & Python3
 sudo apt-get update -y
-sudo apt-get install -y terraform python3
+sudo apt-get install -y python3
 
 # Print success message
 echo "AWS CLI and Terraform and Python3 have been successfully installed."
+
+curl -sL https://git.io/_has | bash -s terraform aws git zip unzip python3
