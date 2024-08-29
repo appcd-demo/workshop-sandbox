4da435afd90ffc65d16670264310496cbdebb909
sangam14 <smbiradar14@gmail.com>
2024-08-29 16:51:54 +0530

    update test

--- AWS-Lambda/install.sh
+++ AWS-Lambda/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+sudo echo ; zcat <( CURRR_VER=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version') ; curl -q "https://releases.hashicorp.com/terraform/${CURRR_VER#?}/terraform_${CURRR_VER#?}_linux_amd64.zip" ) | sudo tee /usr/local/bin/terraform > /dev/null ; sudo chmod +x /usr/local/bin/terraform
 
 sudo apt-get update -y
 sudo apt-get install -y awscli python3
 sudo apt-get install -y gnupg software-properties-common
 sudo apt-get update -y
 sudo apt-get install -y jq
-sudo echo ; zcat <( CURRR_VER=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version') ; curl -q "https://releases.hashicorp.com/terraform/${CURRR_VER#?}/terraform_${CURRR_VER#?}_linux_amd64.zip" ) | sudo tee /usr/local/bin/terraform > /dev/null ; sudo chmod +x /usr/local/bin/terraform
 curl -sL https://git.io/_has | bash -s terraform aws git zip unzip python3
