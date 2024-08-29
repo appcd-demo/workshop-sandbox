61c106bc0e79750d1d3d405786c996bdb11a6e74
sangam14 <smbiradar14@gmail.com>
2024-08-29 16:25:46 +0530

    add install.sh

--- AWS-Lambda/install.sh
+++ AWS-Lambda/install.sh
@@ -1,13 +1,30 @@
-sudo apt install awscli
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+#!/bin/bash
+
+# Update package list and install AWS CLI
+sudo apt-get update -y
+sudo apt-get install -y awscli
+
+# Install required dependencies
+sudo apt-get install -y gnupg software-properties-common
+
+# Add the HashiCorp GPG key
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verify the fingerprint of the key
 gpg --no-default-keyring \
 --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 --fingerprint
+
+# Add the HashiCorp repository
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install terraform
+
+# Update package list and install Terraform & Python3
+sudo apt-get update -y
+sudo apt-get install -y terraform python3
+
+# Print success message
+echo "AWS CLI and Terraform and Python3 have been successfully installed."
